8cc60b9a49261dc639016fb519792c80d1d39277
Deepak <deepakjangir5310@gmail.com>
2020-09-03 01:06:17 +0530

    onClickListener added

--- app/src/main/java/com/deedeveloper/json/MyAdapter.java
+++ app/src/main/java/com/deedeveloper/json/MyAdapter.java
@@ -1,12 +1,12 @@
 package com.deedeveloper.json;
 
 import android.content.Context;
-import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
@@ -35,16 +35,36 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.myViewHolder> {
         holder.textView.setText(datum.getName());
         Glide.with(holder.imageView.getContext()).load(datum.getImage()).into(holder.imageView);
 
-        holder.imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(context,MainActivity2.class);
-                intent.putExtra("Name",datum.getName());
-                intent.putExtra("Image",datum.getImage());
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(intent);
-            }
-        });
+        //RecyclerView onClick listener
+        switch (position){
+            case 0:
+                holder.imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(context, "Image one is clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            case 1:
+                holder.imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(context, "Image two is clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            break;
+        }
+
+
+//        holder.imageView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(context,MainActivity2.class);
+//                intent.putExtra("Name",datum.getName());
+//                intent.putExtra("Image",datum.getImage());
+//                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                context.startActivity(intent);
+//            }
+//        });
     }
 
     @Override
